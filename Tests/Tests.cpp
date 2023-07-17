ee5b93076d059d66089b77acdd88deaf3592bfee
MrNen <sjduttenhefer@gmail.com>
2023-07-17 12:03:56 -0500

    Makes tests not hard-coded and rely on an FL_Data env

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -1,15 +1,18 @@
 #include "pch.h"
 #include "Reader.hpp"
 #include "Structures/FreelancerIni.hpp"
+#include <cstdlib>
+
+const char* flPath = std::getenv("FL_PATH");
 
 TEST(DataTests, FreelancerIniCanBeProcessed)
 {
 	auto ini = Ini::Reader();
-	ini.Parse<Ini::FreelancerIni>(R"(C:\Users\laz\Downloads\shiparch.ini)");
+	ini.Parse<Ini::FreelancerIni>(std::format("{}/DATA/SHIPS/shiparch.ini",flPath));
 }
 
 TEST(DataTests, FreelancerIniCanBeProcessed2)
 {
 	auto ini = Ini::Reader();
-	ini.Parse<Ini::FreelancerIni>(R"(D:\Freelancer\EXE\freelancer.ini)");
+	ini.Parse<Ini::FreelancerIni>(std::format("{}/EXE/Freelancer.ini",flPath));
 }
